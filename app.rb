11dba3bdfc2ac2c896b155712250aec6288d4465
Daniel Wong <wongdaniel8@yahoo.com>
2016-09-07 06:04:24 +0000

    bug fixes

--- app.rb
+++ app.rb
@@ -40,12 +40,14 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game.guess(letter)
-    if letter.nil? or letter == "" or (not letter =~ /[A-Za-z]/)
-      flash[:message] = "Invalid guess."
-    end
-    if @game.guesses.include? letter
-      flash[:message] = "You have already used that letter."
+    bool = @game.guess(letter)
+    if bool == false
+      if letter.nil? or letter == "" or (not letter =~ /[A-Za-z]/)
+        flash[:message] = "Invalid guess."
+      end
+      if @game.guesses.include? letter
+        flash[:message] = "You have already used that letter."
+      end
     end
     redirect '/show'
   end
